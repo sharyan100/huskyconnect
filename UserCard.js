f0c6fb4ae2d2bab4e14914e02c27f0ecb6f4a9ba
sharyan100 <97097001+sharyan100@users.noreply.github.com>
2024-02-03 14:38:33 -0800

    Create UserCard.js

---
+++ UserCard.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const UserCard = ({ name, major, title, content, description, imageSrc, onSwipeLeft, onSwipeRight }) => {
+  const [visible, setVisible] = useState(true);
+
+  const handleSwipeLeft = () => {
+    setVisible(false);
+    onSwipeLeft();
+  };
+
+  const handleSwipeRight = () => {
+    setVisible(false);
+    onSwipeRight();
+  };
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <div className="col-6 col-lg-3">
+      <div className="user mb-4 uw-themed-card">
+        <div className="user-body">
+          <img className="pb-3 profile-image img-fluid" src={imageSrc} alt={`Profile of ${name}`} />
+          <div>
+            <h2>{name}</h2>
+            <p>Major: {major}</p>
+            <h4>{title}</h4>
+            <p>{content}</p>
+            <p>{description}</p> {/* New line to display the description */}
+          </div>
+          <div className="button-container">
+            <button className="swipe-button dont-care" onClick={handleSwipeLeft}>Don't Care</button>
+            <Link to="/match">
+              <button className="swipe-button care" onClick={handleSwipeRight}>Care</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
