7386ce0ec396ec362e3a47344a95cb56a055c29b
sharyan100 <97097001+sharyan100@users.noreply.github.com>
2024-02-03 14:37:52 -0800

    Create MatchedUserCard.js

---
+++ MatchedUserCard.js
@@ -0,0 +1,26 @@
+// MatchedUserCard.js
+import React from 'react';
+
+function MatchedUserCard({ user, onUnmatch, onChat }) {
+  const handleUnmatchClick = () => {
+    onUnmatch(user.name);
+  };
+
+  return (
+    <div className="card">
+      <img className="card-img-top" src={user.img} alt={user.name} />
+      <div className="card-body">
+        <h3 className="card-title">{user.name}</h3>
+        <p>Major: {user.major}</p>
+        <p>Title: {user.title}</p>
+        <p>Content: {user.content}</p>
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary" onClick={handleUnmatchClick}>Unmatch</button>
+          <button type="submit" className="btn btn-primary" onClick={onChat}>Chat</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+  
+export default MatchedUserCard;
